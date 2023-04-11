cf955b9d002ec0968a78b712d61cb05cba69bc00
rn04 <tjs04109@gmail.com>
2023-04-11 18:16:28 +0900

    첫커밋

---
+++ src/main/java/com/khs/exam/javap/JavapApplication.java
@@ -0,0 +1,13 @@
+package com.khs.exam.javap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapApplication.class, args);
+	}
+
+}
